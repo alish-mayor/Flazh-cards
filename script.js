abcdc827783a4e322f48d9dabb49987f2ead6b04
alish-mayor <alish22061999@gmail.com>
2021-09-14 22:29:41 +0600

    added localstorage and data saved

--- script.js
+++ script.js
@@ -18,29 +18,38 @@ const createDeleteBtn = function (card) {
   deleteBtn.innerText = "Delete card";
   deleteBtn.addEventListener("click", (e) => {
     card.remove();
+    updateStorage();
   });
   return deleteBtn;
 };
 
-const createTranslatedCard = function (card) {
+const createTranslatedCard = function (card, translated) {
   const translatedCard = document.createElement("div");
   translatedCard.classList.add("flash-card_translated");
-  translatedCard.innerText = translateInput.value;
+  translatedCard.innerText = translated;
   card.addEventListener("click", (e) => {
     translatedCard.classList.toggle("visible");
   });
   return translatedCard;
 };
 
-const createCard = function () {
+const createCard = function (cardEl) {
+  let originalText = originalInput.value;
+  let translateText = translateInput.value;
+
+  if (cardEl) {
+    originalText = cardEl.original;
+    translateText = cardEl.translated;
+  }
+
   const card = document.createElement("div");
   card.classList.add("flash-card");
   const html = `<h2 class="card__title">Original word</h2>
-    <p class="card__text">${originalInput.value}</p>`;
+    <p class="card__text">${originalText}</p>`;
   card.insertAdjacentHTML("beforeend", html);
   const deleteBtn = createDeleteBtn(card);
   card.appendChild(deleteBtn);
-  const translatedCard = createTranslatedCard(card);
+  const translatedCard = createTranslatedCard(card, translateText);
   card.appendChild(translatedCard);
 
   mainContent.appendChild(card);
@@ -51,6 +60,20 @@ const clearInputs = function () {
   translateInput.value = "";
 };
 
+const updateStorage = function () {
+  const cardEl = document.querySelectorAll(".flash-card");
+
+  const cards = [];
+  cardEl.forEach((card) => {
+    cards.push({
+      original: card.querySelector("p").textContent,
+      translated: card.querySelector("div").textContent,
+    });
+  });
+
+  localStorage.setItem("cards", JSON.stringify(cards));
+};
+
 // Event listeners
 
 form.addEventListener("submit", (e) => {
@@ -58,6 +81,15 @@ form.addEventListener("submit", (e) => {
   createCard();
   clearInputs();
   closeModal();
+  updateStorage();
 });
 
 openAddCard.addEventListener("click", openModal);
+
+const cards = JSON.parse(localStorage.getItem("cards"));
+
+if (cards) {
+  cards.forEach((card) => {
+    createCard(card);
+  });
+}
